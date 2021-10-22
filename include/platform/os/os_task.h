98024dcaaf9f236d2f40a6bb8cd75f2a806b141f
leon <372588334@qq.com>
2021-10-22 11:06:09 +0800

    change.

--- include/platform/os/os_task.h
+++ include/platform/os/os_task.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define CONFIG_os_task_MM_TRACE 1
+#define CONFIG_OS_TASK_MM_TRACE 1
 
 typedef struct os_task_mm_node_s{
 	struct list_head list;
@@ -24,24 +24,6 @@ typedef struct os_task_mm_node_s{
 typedef void *(*os_task_func_t)(void *user_data);
 typedef struct os_task_s os_task_t;
 
-typedef struct {
-	const char *name;
-	/* task ops */
-	os_task_t *(*create)(const char *name,unsigned long stack_size,int priority,os_task_func_t func,void *arg);
-	os_task_t *(*create2)(const char *name,unsigned long stack_size,int priority,int async,os_task_func_t func,void *arg);
-
-	/* mem ops */
-	void (*mm_show)(void);
-	void (*mm_add)(unsigned long tid,os_task_mm_node_t *mnode);
-	void (*mm_del)(unsigned long tid,void *addr);
-	
-	/* misc */
-	mt_async_queue_t *(*aq_get)(const char *name);
-	mt_async_queue_t *(*aq_self)(void);
-	const char *(*name_get_from_pid)(unsigned long pid);
-	const char *(*name_get_from_tid)(unsigned long tid);
-	
-}os_task_ops;
 
 os_task_t *os_task_create(const char *name,unsigned long stack_size,int priority,os_task_func_t func,void *arg);
 os_task_t *os_task_create2(const char *name,unsigned long stack_size,int priority,int async,os_task_func_t func,void *arg);
