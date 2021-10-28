f3c3900198a567d667a994be2851230e75e43ce8
leon <372588334@qq.com>
2021-10-28 11:22:05 +0800

    new OS thread API

--- include/platform/os/os_thread.h
+++ include/platform/os/os_thread.h
@@ -7,8 +7,19 @@ extern "C" {
 #endif
 
 
+typedef void* os_pthread_t;
+
+typedef struct os_pthread_attr_s {
+	int res;
+}os_pthread_attr_t;
 
 
+///////////////////////////////////////////////////////////////////
+int os_pthread_create(os_pthread_t *tid, const os_pthread_attr_t *attr, void *(*func) (void *), void *arg);
+int os_pthread_join (os_pthread_t tid, void ** status);
+os_pthread_t os_pthread_self (void);
+int os_pthread_detach (os_pthread_t tid);
+void os_pthread_exit (void *status);
 
 #ifdef  __cplusplus
 }
--- platform/os/linux/os_thread.c
+++ platform/os/linux/os_thread.c
@@ -1,6 +1,32 @@
 
+#include <pthread.h>
+#include "os_thread.h"
 
 
+int os_pthread_create(os_pthread_t *tid, const os_pthread_attr_t *attr, void *(*func) (void *), void *arg)
+{
+	return 0;
+}
+
+int os_pthread_join (os_pthread_t tid, void ** status)
+{
+	return 0;
+}
+
+os_pthread_t os_pthread_self (void)
+{
+	return NULL;
+}
+
+int os_pthread_detach (os_pthread_t tid)
+{
+	return 0;
+}
+
+void os_pthread_exit (void *status)
+{
+
+}
 
 
 
