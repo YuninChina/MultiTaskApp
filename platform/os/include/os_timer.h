db0a5eacb8ed18859395486ae5cf6aa15d48a7fe
leon <372588334.com>
2022-01-24 05:48:43 -0800

    <feature>: 增加timer功能.

---
+++ platform/os/include/os_timer.h
@@ -0,0 +1,31 @@
+
+
+
+#ifndef __OS_TIMER__H
+#define __OS_TIMER__H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+typedef struct os_timer_s 
+{
+	unsigned long long start;
+	unsigned long long end;
+}os_timer_t;
+
+
+void os_timer_start(os_timer_t *timer);
+unsigned int os_timer_stop(os_timer_t *timer);
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif 
+
+
+
+
+
---
+++ platform/os/linux/base/os_timer.c
@@ -0,0 +1,50 @@
+#include <time.h>
+#include <stdio.h>
+#include <strings.h>
+
+
+#include "os_timer.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+long long get_monotonic_time (void)
+{
+	struct timespec ts;
+	clock_gettime (CLOCK_MONOTONIC, &ts);
+	return (((long long) ts.tv_sec) * 1000000) + (ts.tv_nsec / 1000);
+}
+
+
+void os_timer_start(os_timer_t *timer)
+{
+	if(timer)
+	{
+		bzero(timer,sizeof(*timer));
+		timer->start = get_monotonic_time();
+	}
+}
+
+unsigned int os_timer_stop(os_timer_t *timer)
+{
+	unsigned int time = 0;
+	long long elapsed = 0;
+	if(timer)
+	{
+		timer->end = get_monotonic_time();
+		elapsed = timer->end - timer->start;
+		time = elapsed % 1000000;
+	}
+	return time;
+}
+
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+
